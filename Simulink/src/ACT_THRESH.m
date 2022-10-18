3c968d7d1ccc31c5ce9c96828ccdfbcb09ee1719
l0ckay <59676067+l0ckay@users.noreply.github.com>
2022-10-18 00:48:14 -0400

    creates enumerations for activity threshold and modes

---
+++ Simulink/src/ACT_THRESH.m
@@ -0,0 +1,12 @@
+classdef ACT_THRESH < Simulink.IntEnumType
+    enumeration
+        VLOW(0)
+        LOW(1)
+        MEDLOW(2)
+        MED(3)
+        MEDHIGH(4)
+        HIGH(5)
+        VHIGH(6)
+    end
+end
+
---
+++ Simulink/src/MODE.m
@@ -0,0 +1,23 @@
+classdef MODE < Simulink.IntEnumType
+    enumeration
+        Off(0)
+        DDD(1)
+        VDD(2)
+        DDI(3)
+        DOO(4)
+        AOO(5)
+        AAI(6)
+        VOO(7)
+        VVI(8)
+        AAT(9)
+        VVT(10)
+        DDDR(11)
+        VDDR(12)
+        DDIR(13)
+        DOOR(14)
+        AOOR(15)
+        AAIR(16)
+        VOOR(17)
+        VVIR(18)
+    end
+end
